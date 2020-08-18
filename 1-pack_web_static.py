621d877ab95f97be4e3a94fb0484d00d632718ab
Jilroge7 <1672@holbertonschool.com>
2020-08-18 01:09:27 +0000

    fix to tgz

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,9 +8,9 @@ def do_pack():
     """function to generate the .tgz """
     local("mkdir -p versions")
     created = (time.strftime("%Y%m%d%H%M%S"))
-    compressed = local("tar -cvzf versions/web_static_{}.tzg web_static"
+    compressed = local("tar -cvzf versions/web_static_{}.tgz web_static"
                        .format(created))
     if compressed.succeeded:
-        return "versions/web_static_{}.tzg".format(created)
+        return "versions/web_static_{}.tgz".format(created)
     else:
         return None
