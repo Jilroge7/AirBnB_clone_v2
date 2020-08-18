2b0a2795d62241a7311b5399470152cbd1fe5482
Jilroge7 <1672@holbertonschool.com>
2020-08-18 00:41:51 +0000

    adding task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" script that generates .tgz archive frm contents of web_static """
+from fabric.api import *
+import time
+
+
+def do_pack():
+    """function to generate the .tgz """
+    try:
+        local("mkdir -p versions")
+        created = (time.strftime("%Y%m%d%H%M%S"))
+        return local("tar -cvzf versions/web_static_{} web_static"
+                     .format(created))
+    except:
+        return None
