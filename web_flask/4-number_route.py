295769b85ab5a919dce2cd99d898e4466e644b96
Jilroge7 <1672@holbertonschool.com>
2020-09-01 01:33:27 +0000

    adding task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" Script to begin a flask application """
+from flask import Flask
+from string import Template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_route():
+    """ view resp to display hello whn the strtd app is curled """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_route():
+    """ view resp to display hbnb whn routed to /hbnb """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """ view resp display value of text whn routed to /c/.. """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text="is cool"):
+    """ view resp display phrase whn routed to python """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def is_number(n):
+    """ display if route is number or not """
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
