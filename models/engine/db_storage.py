804f90a8b8b657c72b6b93570f0882505dabcbbb
Jilroge7 <1672@holbertonschool.com>
2020-09-03 04:48:52 +0000

    adding shebang

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+""" db storage """
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker, scoped_session
